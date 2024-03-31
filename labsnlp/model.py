465543b3be54d5ecbaf3c940cb36fa4dc95c1880
Dariienko <dariienko.dima@gmail.com>
2024-03-31 12:52:23 +0300

    train/dev/test.split, cv, attempt to use PCA, ROC-AUC BEST_MODEL: 0.814006895090104

--- labsnlp/model.py
+++ labsnlp/model.py
@@ -1,8 +1,9 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.calibration import CalibratedClassifierCV
 
-
-def train_gaussian_nb_classifier(X, y):
+def train_gaussian_nb_classifier(X, y, cv):
     clf = GaussianNB()
-    clf.fit(X, y)
+    clf_CV = CalibratedClassifierCV(clf, cv = cv)
+    clf_CV.fit(X, y)
 
-    return clf
+    return clf_CV
